300f2ed411f246073d40fa3fd1b260079cd7862a
Vinh Nguyen <nguyenducvinh92@gmail.com>
2015-08-05 09:18:55 +0700

    I1-static-page

---
+++ app/assets/javascripts/static_pages.js.coffee
@@ -0,0 +1,2 @@
+$(document).ready ->
+  $("div.alert").delay(3000).slideUp()
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = t "static_page.brs"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
