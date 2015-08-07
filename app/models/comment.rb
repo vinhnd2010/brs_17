af1da48a94eea7e661448669fd772e538166c19a
hellovietnam93 <hellovietnam93@gmail.com>
2015-08-07 09:35:27 +0700

    Create activity

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,16 @@
 class Comment < ActiveRecord::Base
+  include ActivityLog
+
   belongs_to :review
   belongs_to :user
 
-  validates :content, presence: true, length: {minimum: 10,
-    too_short: "must have at least %{count} words"}
+  validates :content, presence: true, length: {minimum: Settings.validates.comment.minlength,
+    too_short: I18n.t("flash.validates.tooshort", count: count)
+
+  after_save :create_commented_activity
+
+  private
+  def create_commented_activity
+    create_activity user_id, review_id, Settings.activities.commented
+  end
 end
