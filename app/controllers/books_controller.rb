02daa1f5b133135279bfd98565073be505d2787b
trungnn142 <trungnn142@gmail.com>
2015-08-10 13:52:56 +0700

    finish create book detail and modify a little layout

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,19 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!, only: :index
+  before_action :authenticate_user!, only: [:index, :show]
+  before_action :load_book, only: :show
 
   def index
     @search = Book.search params[:q]
     @search.build_condition if @search.conditions.empty?
     @search.build_sort if @search.sorts.empty?
     @books = @search.result.paginate page: params[:page], per_page: Settings.paginate_per_page
+  end
+
+  def show
+  end
+
+  private
+  def load_book
+    @book = Book.friendly.find params[:id]
   end
 end
