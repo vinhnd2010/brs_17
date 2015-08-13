58d336242f6ed9283dbf68e228a7a5cfd0add081
Vinh Nguyen <nguyenducvinh92@gmail.com>
2015-08-13 10:49:17 +0700

    Finish send email noti accepted request

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,13 @@
 class Request < ActiveRecord::Base
   belongs_to :user
+  after_save :sent_email
 
   enum status: [:wait, :accept, :decline]
+
+  private
+  def sent_email
+    if self.accept?
+      RequestMailer.acception_request(self).deliver_now
+    end
+  end
 end
