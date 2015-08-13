f0e519ac7110ccbbec1d54ad0349713f9d2bc5ad
Vinh Nguyen <nguyenducvinh92@gmail.com>
2015-08-13 15:41:02 +0700

    Finish delete accepted request

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,8 @@ Rails.application.configure do
     user_name: ENV["EMAIL"],
     password: ENV["PASS"],
     authentication: :plain,
-    enable_starttls_auto: true
+    enable_starttls_auto: true,
+    openssl_verify_mode: "none"
   }
 
   config.active_support.deprecation = :log
