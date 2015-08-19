74b5617ca46a1f2f1a2c68bf529f247232e5e06f
hellovietnam93 <hellovietnam93@gmail.com>
2015-08-19 08:36:03 +0700

    Test comment controller

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -4,5 +4,12 @@ FactoryGirl.define do
     book
     content "dsfsdfsdf sdfdsfdsfdsafs"
     score [0,1,2,3,4,5].sample
+
+    factory :content_too_short do
+      content "aa"
+      user
+      book
+      score [0,1,2,3,4,5].sample
+    end
   end
 end
